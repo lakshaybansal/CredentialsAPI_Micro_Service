9b67f365c3a9ad9f844202c310f5208b5847150a
tyda <vinit@tyda.in>
2016-06-09 17:57:23 +0530

    JWT Part Done (Validate, Decode)

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config.json'); // get our config file
 
 var app = express();
-app.use(cors());
+//app.use(cors())
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "http://localhost:63342");
+   res.header('Access-Control-Allow-Credentials', true);
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   next();
+});
 
 mongoose.connect(config.database);
 
@@ -17,7 +24,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    console.log("connected");
+   console.log("connected");
 });
 
 app.use(logger('dev'));
--- routes/credentials.api.js
+++ routes/credentials.api.js
@@ -1,102 +1,118 @@
 var express = require('express');
 var apiRoutes = express.Router();
-var userCredential = require('../models/usercredentials.model');
+var userCredential = require('../models/userCredentials.model.js');
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('../config'); // get our config file
 var request = require("request");
 var Q = require('q');
 
 var superSecret = config.secret;
+var profile_API_address = config.api.profileAPI;
 
 apiRoutes.post('/authenticate', function (req, res) {
-    userCredential.authenticate(req.body).then(function (response) {
-        getProfile(req.body.username).then(function (data) {
-            res.send(data);
-        }).catch(function () {
-            res.sendStatus(400);
-        });
-    }).catch(function (response) {
-        res.sendStatus(400);
-    })
+   userCredential.authenticate(req.body).then(function (response) {
+      getProfile(req.body.username).then(function (data) {
+         res.send(data);
+      }).catch(function () {
+         res.send({status: false, reason: 'Error is Fetching Profile'});
+      });
+   }).catch(function (response) {
+      res.send(response);
+   });
 });
 
 function getProfile(username) {
-    var deferred = Q.defer();
-    request("http://172.23.238.164:3040/api/userEntity/" + username, function (error, response, body) {
-        if (error) {
-            deferred.reject()
-        }
-        else if (body == 'null') {
-            deferred.reject();
-        }
-        else if (response.statusCode == 400) {
-            deferred.reject();
-        }
-        else if (!error && response.statusCode == 200 && body != null) {
-            var data = JSON.parse(body);
-            var userObj = {
-                username: username,
-                band: data.band,
-                work_experience_in_year: data.work_experience_in_year,
-                country_code: data.country_code,
-                country: data.country,
-                preferred_location: data.preferred_location
-            };
-            genrateJWT(userObj).then(function (token) {
-                deferred.resolve(token);
-            });
+   var deferred = Q.defer();
+   request(profile_API_address + username, function (error, response, body) {
 
-        }
-    });
-    return deferred.promise;
+      if (error) {
+         deferred.reject()
+      }
+      else if (body == 'null') {
+         deferred.reject();
+      }
+      else if (response.statusCode == 404) {
+         deferred.reject();
+      }
+      else if (!error && response.statusCode == 200 && body != null) {
+         var data = JSON.parse(body);
+         var userObj = {
+            username: username,
+            band: data.band,
+            work_experience_in_year: data.work_experience_in_year,
+            country_code: data.country_code,
+            country: data.country,
+            preferred_location: data.preferred_location
+         };
+         genrateJWT(userObj).then(function (token) {
+            deferred.resolve(token);
+         });
+
+      }
+   });
+   return deferred.promise;
 }
 
 function genrateJWT(obj) {
-    var deferred = Q.defer();
-    var token = jwt.sign(obj, 'vinitKumar', {
-        expiresIn: 86400 // expires in 24 hours
-    });
+   var deferred = Q.defer();
+   var token = jwt.sign(obj, superSecret, {
+      expiresInMinutes: 60 * 120  // expires in 120 Minutes
+   });
 
-    deferred.resolve(token);
+   deferred.resolve(token);
 
-    return deferred.promise;
+   return deferred.promise;
 }
 
 apiRoutes.post('/register', function (req, res, next) {
-    userCredential.register(req.body).then(function () {
-        res.sendStatus(200);
-    }).catch(function () {
-        res.sendStatus(400);
-    });
+   userCredential.register(req.body).then(function () {
+      res.sendStatus(200);
+   }).catch(function () {
+      res.sendStatus(400);
+   });
 });
 
-apiRoutes.post('/verifytoken', function (req, res, next) {
-    var token = req.body.token || req.params('token') || req.headers['x-access-token'];
-    if (token) {
-        jwt.verify(token, superSecret, function (err, decoded) {
-            if (err) {
-                return res.json({success: false, message: 'Failed to authenticate token.'});
-            } else {
-                req.decoded = decoded;
-                res.send(req.decoded);
-                //res.send("Hello");
-            }
-        });
+apiRoutes.get('/verifyToken', function (req, res, next) {
+   var token = req.cookies.token;
+   if (token) {
+      fetchToken(token).then(function () {
+         res.send({status: true});
+      }).catch(function () {
+         res.send({status: false});
+      });
+   } else {
+      res.send({status: false});
+   }
+});
 
-    } else {
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
+apiRoutes.get('/decodeToken', function (req, res, next) {
+   var token = req.cookies.token;
+   if (token) {
+      fetchToken(token).then(function (obj) {
+         res.send({object: obj});
+      }).catch(function () {
+         res.send({status: false});
+      });
+   } else {
+      return res.send({
+         status: false,
+         message: 'No token provided.'
+      });
 
-    }
+   }
 
 });
-apiRoutes.use(function (req, res) {
-    var token = jwt.sign(req.body, superSecret, {
-        expiresIn: 86400 // expires in 24 hours
-    });
-    res.send(token);
-});
+
+function fetchToken(token) {
+   var deferred = Q.defer();
+   jwt.verify(token, superSecret, function (err, decoded) {
+      if (err) {
+         deferred.reject({status: false, message: 'Failed to authenticate token.'});
+      } else {
+         deferred.resolve(decoded);
+      }
+   });
+   return deferred.promise;
+}
 
 module.exports = apiRoutes;
