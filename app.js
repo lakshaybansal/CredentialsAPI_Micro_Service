f6410f574f3249afcbc1f3cfefc172f0a99b1395
Nishant Jain <nishantkumarajain@gmail.com>
2016-06-15 03:02:04 +0530

    added access control allow origin

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var app = express();
 
 var config = require('./config.json'); // get our config file
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "http://localhost:63342");
-   res.header('Access-Control-Allow-Credentials', true);
+   res.header("Access-Control-Allow-Origin", "http://localhost:8090");
+   res.header("Access-Control-Allow-Credentials",true);
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
    next();
